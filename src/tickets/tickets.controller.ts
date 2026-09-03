0ead504afadd60955c629acd6a003191f33f634e
Diego Salvador Pérez Carrillo <tizkeyy7@gmail.com>
2026-09-02 20:55:32 -0600

    Fix JwStrategy secret loading using ConfigService

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,11 +1,13 @@
-import {Controller, Get, Post, Body } from '@nestjs/common'
+import {Controller, Get, Post, Body, UseGuards } from '@nestjs/common'
 import { TicketsService } from './tickets.service'
 import { CreateTicketDto } from './create-ticket.dto'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('tickets')
 export class TicketsController {
     constructor(private readonly ticketsService : TicketsService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     crear(@Body() dto:CreateTicketDto) {
         return this.ticketsService.crear(dto);
@@ -16,6 +18,7 @@ export class TicketsController {
         return this.ticketsService.verCola();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post('atender')
     atenderSiguiente() {
         return this.ticketsService.atenderSiguiente();
